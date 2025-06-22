6e75531d68ba3d40ec7a9795e981a905948887bb
mukeshgeetha46 <mukeshgeetha46@gmail.com>
2025-06-22 13:26:08 +0530

    add new image

--- src/pages/Table/Maintable2.jsx
+++ src/pages/Table/Maintable2.jsx
@@ -5,43 +5,84 @@ import palystatios from '../../assets/Main/product-7-Bvj-91bc.png'
 import chair from '../../assets/Main/product-8-BbgjC3Cu.png'
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { useState } from "react"
+import customer1 from '../../assets/Main/user1-D00qJYN4.jpg'
+import customer2 from '../../assets/Main/user2-CcvFtdw4.jpg'
+import customer3 from '../../assets/Main/user3-CoJW3aN6.jpg'
+import customer4 from '../../assets/Main/user4-D8sJ5_HA.jpg'
+import customer5 from '../../assets/Main/user5-JxJV3ECH.jpg'
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+
 
 const MainTable2 = () => {
 
     const tableHead = ["checkbox","Products","customer","Reviews","Status","Time","Action"];
     const [openMenuIndex, setOpenMenuIndex] = useState(null);
 
+    const customers = [customer1, customer2, customer3, customer4, customer5];
+
+     
+
     const tableData = [
         {
            productImg:iphone,
            productname:"iPhone 13 pro max",
+           customerDetail:{
+            name:'Arlene McCoy',
+            email:'macoy@arlene.com'
+           }
         },
         {
            productImg:applemac,
            productname:"Apple MacBook Pro",
+           customerDetail:{
+            name:'Jerome Bell',
+            email:'belljerome@yahoo.com'
+           }
         },
         {
            productImg:palystatios,
            productname:"PlayStation 5 DualS",
+           customerDetail:{
+            name:'Jacob Jones',
+            email:'jones009@hotmail.com'
+           }
         },    
         {
            productImg:chair,
            productname:"Amazon Basics ...",
+           customerDetail:{
+            name:'Annette Black',
+            email:'blackanne@yahoo.com'
+           }
         },
     ];
 
-    const LevelLoader = () => {
+    const LevelLoader = ({ userImg,users }) => {
         return (
             <div className='title flex-col justify-start items-center gap-3'>
-                <p>$180/ 499</p>
-                <p>Partially paid</p>
-                <div className="flex h-[7px] w-[100px] mt-2">
-                    <div className="w-2/3 bg-[#0085db] rounded-tl-2xl rounded-bl-2xl"></div>
-                    <div className="w-1/6 bg-[#f5f8fb] rounded-tr-2xl rounded-br-2xl"></div>
-                </div>
+             
+                  <div className="flex items-center">
+                    <img className="w-10 h-10 flex items-center justify-center rounded-full hover:bg-gray-300 cursor-pointer transition" src={userImg} />  
+                    
+                    <div>
+                        <p>{users.name}</p>
+                        <p>{users.email}</p>
+                    </div>
+                  </div>
             </div>
         )
     };
+
+    const Reviews = ({ offstar,fullstar }) => {
+        return (
+            <div>
+                <p className="flex"><AiFillStar color="#f8c076" /><AiFillStar color="#f8c076" /><AiFillStar color="#f8c076" /><AiFillStar color="#f8c076" /><AiOutlineStar color="#f8c076" /></p>
+                <p className="text-wrap">
+                    This theme is great. Clean and easy to understand....
+                </p>
+            </div>
+        )
+    }
 
     return (
         <div className="overflow-x-auto">
@@ -73,12 +114,18 @@ const MainTable2 = () => {
                                         </div>
                                     </td>
                                     <td className="px-4 py-4">
-                                        <LevelLoader />
+                                        <LevelLoader userImg={customers[index]} users={td.customerDetail} />
+                                    </td>
+                                    <td className="px-4 py-4 w-[230px]">
+                                        <Reviews fullstar={4} offstar={1} />
                                     </td>
                                     <td className="px-4 py-4 text-green-600 flex-col justify-start items-center">
                                         <p className='pt-[1px] pb-[3px] bg-[#F1EBFF] text-[#8763DA] flex justify-center items-center rounded-2xl text-[12px]'>
                                             Confirmed
                                         </p>
+                                    </td>
+                                    <td className="px-4 py-4 flex-col justify-start items-center">
+                                        <p>Nov 8</p>
                                     </td>
                                     <td className="px-4 py-4 relative">
                                         <button
